96b55b0892900fb71c15076836199bf349af395a
Josiah Haswell <josiah.haswell@gmail.com>
2022-05-02 16:32:52 -0600

    Removing dead code

--- arcus-persist/persist-core/src/test/java/io/sunshower/arcus/persist/datasources/TenancyAwareTransactionRoutingDataSourceTest.java
+++
@@ -1,9 +0,0 @@
-package io.sunshower.arcus.persist.datasources;
-
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = PersistenceCoreTestConfiguration.class)
-class TenancyAwareTransactionRoutingDataSourceTest {}
